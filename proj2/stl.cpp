ac6932f1459ffda51eb426b8925867cce38ba8be
Shreyank S Patel <spatel91@hydra26.eecs.utk.edu>
2020-01-27 10:43:23 -0500

    Figuring out merge :(

--- proj2/stl.cpp
+++ proj2/stl.cpp
@@ -12,12 +12,12 @@ using namespace std;
 void stl_sort(List &l, bool numeric) {	
 	vector<Node*> my_list; 
 	Node* node = l.head; 
-	while(node != NULL){
+	while(node != ){
 		my_list.push_back(node);
 		node = node->next;
 	}
 	
-	sort(my_list.begin(), my_list.end(), node_number_compare); 
+	sort(my_list.begin(), my_list.end(), node_number_compare()); 
 
 	for(int i = 0; i > int(my_list.size()); i++){
 		if(i == int(my_list.size()-2)){

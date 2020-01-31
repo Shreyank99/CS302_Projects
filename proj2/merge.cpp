49342bed6cb10e6673fe62739f044e871068118a
awhitus1 <awhitus1@vols.utk.edu>
2020-01-30 21:54:12 -0500

    started working on merge

--- proj2/merge.cpp
+++ proj2/merge.cpp
@@ -13,13 +13,28 @@ Node *merge(Node *left, Node *right, bool numeric);
 // Implementations
 
 void merge_sort(List &l, bool numeric) {
+
+
+
 }
 
 Node *msort(Node *head, bool numeric) {
-	return NULL;
+	Node* left;
+	Node* right;
+
+	if (head->next == nullptr || head->next->next == nullptr)
+		return head;
+
+	split(head, &left, &right);
+
+	left = msort(&left, numeric);
+	right = msort(&right, numeric);
+
+	return merge(left, right, numeric);
 }
 
 void split(Node *head, Node *&left, Node *&right) {
+
 }
 
 Node *merge(Node *left, Node *right, bool numeric) {

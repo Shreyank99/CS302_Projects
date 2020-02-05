ee6f9057b86a39d1bbaa2c86f058f4b673a69c95
awhitus1 <awhitus1@vols.utk.edu>
2020-02-04 22:46:18 -0500

    starting benchmark

---
+++ proj2/benchmarking/input_gen.py
@@ -0,0 +1,19 @@
+# input_gen.py
+
+# outputs a quantity of random ints according to a argument
+# run input_gen.pu -h for more info
+
+import argparse
+import random
+
+parser = argparse.ArgumentParser(description='generate random ints')
+parser.add_argument('count', metavar='N', type=int, nargs=1,
+                   help='the number of ints to output')
+
+args = parser.parse_args()
+count = args.count[0]
+
+randList = [random.randint(0, 200000000) for i in range(count)]
+
+for i in randList:
+    print(i)
